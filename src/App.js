ac8ab5dbc3d29a06e2a559f234564e0340ced1cd
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-01-01 19:32:18 +0600

    initializeMyWork

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home/Home';
 function App() {
   return (
     <Home />
+    
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='text-5xl'>
             This is home
         </div>
     );
