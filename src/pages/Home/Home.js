584396b84106f9b647243e6eb6bea7a8626b9921
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-01-02 12:02:49 +0600

    'experience-progressbar-add'

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Experience from './Experience/Experience';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Experience />
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors:{
+      'purple': '#818cf8',
+      'amber' : '#fbbf24',
+      'indigo': '#4f46e5'
+    },
   },
   plugins: [],
 }
