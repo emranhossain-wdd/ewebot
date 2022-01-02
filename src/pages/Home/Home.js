0a7d459307c40b15fb9838fc99393dcf91f1039c
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-01-02 19:17:21 +0600

    'feature-project'

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Experience from './Experience/Experience';
+import Feature from './Feature/Feature';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Experience />
+            <Feature />
         </div>
     );
 };
