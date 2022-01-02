d78c7fa7ebb0989021b1798ba27d1ae5224f403c
KanizNUBTK <kaniz.m.mim@gmail.com>
2022-01-02 12:07:55 +0600

    Experience Down Part commited

---
+++ src/pages/ExperienceDownPart/ExperienceDownPart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExperienceDownPart = () => {
+    return (
+        <div>
+            <h1>ExperienceDownPart</h1>
+        </div>
+    );
+};
+
+export default ExperienceDownPart;
